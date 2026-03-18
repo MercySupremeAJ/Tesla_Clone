fbf3a2544b0e41eec9b0e9b493b4dc84b93728a5
Mercy Ajoke Supreme <mercy.oghenewoakpo@gmail.com>
2026-03-18 14:30:22 +0100

    fix: prevent Vercel build fail by ignoring unused imports in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
 import logo from './logo.svg';
+// eslint-disable-next-line no-unused-vars
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header'
